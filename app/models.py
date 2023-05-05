2c7635bc6bef43ccb35f36c9cb94a17718c30361
Allyson <allysonpaulino23@gmail.com>
2023-05-05 08:55:06 -0300

    logics water intake

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+from django.contrib.auth.models import User
+
 
 class Usuario(AbstractBaseUser):
     nome = models.CharField(max_length=55, null=False, blank=False)
@@ -12,3 +15,58 @@ class Usuario(AbstractBaseUser):
     def __str__(self):
         return self.email
     
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(
+        'criado em',
+        auto_now_add=True,
+        auto_now=False
+    )
+    modified = models.DateTimeField(
+        'modificado em',
+        auto_now_add=False,
+        auto_now=True
+    )
+    
+    class Meta:
+        abstract = True
+        
+        
+MOVIMENTO = (
+    ('e', 'entrada'),
+    ('s', 'saida'),
+)
+
+class Estoque(TimeStampedModel):
+    COPO_1 = 1
+    COPO_2 = 2
+
+    INTEGER_CHOICES = (
+        (COPO_1, 'Primeiro Valor'),
+        (COPO_2, 'Segundo Valor'),
+    )
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    copo = models.CharField(choices=INTEGER_CHOICES, max_length=8)
+    
+    class Meta:
+        ordering = ('-created',)
+        
+    def __str__(self):
+        return str(self.pk)
+    
+class ConsumoAgua(models.Model):
+    COPO_1 = 1
+    COPO_2 = 2
+    
+    INTEGER_CHOICES = (
+    (COPO_1, 'Primeiro Valor'),
+    (COPO_2, 'Segundo Valor'),
+    )
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE)
+    quantidade = models.CharField(choices=INTEGER_CHOICES, max_length=8)
+    
+    class Meta:
+        ordering=('pk',)
+    
+    def __str__(self):
+        return '{} - {}'.format(self.pk, self.quantidade)
